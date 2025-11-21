8df7b840727141a5664bec2839eb5f431d183974
ddop711 <ddop711@gmail.com>
2025-11-21 22:03:33 +0100

    completely done done done

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ helmet.addEventListener("click", click);
 function click() {
   overskrift.textContent = "Helmet";
   underrubrik.textContent = "Hvis vi skal op i mod Enderdragen, skal vi udryste os selv med hjelme og brynjer, for at undgå at dø.";
-  tekst1.innerHTML = `<img src="./EK/04_animation/the_ender_dragon/img/helmet.webp" alt="helmet";">`;
+  tekst1.innerHTML = `<img src="img/helmet.webp" alt="helmet";">`;
 }
 
 //Sword
@@ -40,7 +40,7 @@ sword.addEventListener("click", clickSword);
 function clickSword() {
   overskrift.textContent = "Sværd";
   underrubrik.textContent = "For at skade Enderdragen skal vi bruge våben. Få fat i et sværd, gerne af netherite, men diamant er også fint.";
-  tekst1.innerHTML = `<img src="./EK/04_animation/the_ender_dragon/img/sword.png" alt="sword";">`;
+  tekst1.innerHTML = `<img src="img/sword.png" alt="sword";">`;
 }
 
 //Apple
@@ -58,6 +58,6 @@ apple.addEventListener("click", clickApple);
 function clickApple() {
   overskrift.textContent = "Gyldne Æble";
   underrubrik.textContent = "Vi skal i krig, så det er vigtigt at have spist godt inden, eller imens.";
-  tekst1.innerHTML = `<img src="./EK/04_animation/the_ender_dragon/img/goldenapple.webp" alt="apple";">
+  tekst1.innerHTML = `<img src="img/goldenapple.webp" alt="apple";">
   `;
 }
